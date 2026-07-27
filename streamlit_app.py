e5618640048dbef44b15565ea2bfcdff1e34c75f
santamarym <santamarym04@gmail.com>
2026-07-27 11:05:04 +0530

    Add Streamlit wrapper for deployment

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(page_title="SupportX", layout="wide")
+
+with open("frontend/index.html", "r", encoding="utf-8") as f:
+    html_content = f.read()
+
+components.html(html_content, height=900, scrolling=True)
